4f853f065430b063cff970fdc671e3ca3c94a746
sergiosainzcommx <s3rgio.sainz@gmail.com>
2021-07-10 02:43:39 -0500

    Se agrega verificación y limpiar el canvas

--- dibujo.js
+++ dibujo.js
@@ -2,6 +2,7 @@
 var texto = document.getElementById("texto_lineas");
 /*  boton es un OBJETO*/
 var boton = document.getElementById("botoncito");
+var limpiar = document.getElementById("limpiando");
 /*  El OBJETO boton tiene un evento "click" */
 boton.addEventListener("click", dibujoPorClick);
 
@@ -18,47 +19,46 @@ var ancho = d.width;
    que será un dibijo en 2D. E   getContext en un MÉTODO del OBJETO canvas      */
 var lienzo = d.getContext("2d"); 
 
-/* Crear una función que dibuje una linea                                       */
+// Crear una función que dibuje una linea                                       
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
-    /* Le digo a JS que arrancaré un trazo                                      */
-    lienzo.beginPath();
     
-    /* Le digo que color será la linea                                          */
-    lienzo.strokeStyle = color;
-
-    /* Indico de donde va iniciar el trazo                                      */
-    lienzo.moveTo(xinicial, yinicial); 
-
-    /* Indico que trazaré una linea hasta un punto 200,200                      */
-    lienzo.lineTo(xfinal, yfinal);
-
-7    /* Es el método que dibujará la linea                                       */
-    lienzo.stroke();
-
-    /* Le indico que pare el trazo                                              */
-    lienzo.closePath();
+    lienzo.beginPath();                 /* Le digo a JS que arrancaré un trazo                                      */
+    lienzo.strokeStyle = color;         /* Le digo que color será la linea                                          */
+    lienzo.moveTo(xinicial, yinicial);  /* Indico de donde va iniciar el trazo                                      */
+    lienzo.lineTo(xfinal, yfinal);      /* Indico que trazaré una linea hasta un punto 200,200                      */
+    lienzo.stroke();                    /* Es el método que dibujará la linea                                       */    
+    lienzo.closePath();                 /* Le indico que pare el trazo                                              */
 }
+
 
 function dibujoPorClick()
 {
-    /*  Para obtener el valor de un formulario llamo al ATRIBUTO del OBJETO texto
-        y conviento el texto en entero con la función parseInt */
     var lineas = parseInt(texto.value);
-    var l = 0;
-    var  xi, yi, xf, yf ;
-    var espacio = ancho / lineas; 
-
-    while(l < lineas)
+    var valor = lineas * 1;
+    if(isNaN(valor))
     {
-        xi = espacio * l;
-        yi = espacio * l;
-        xf = espacio * (l + 1);
-        yf = espacio * (l + 1);
+        alert("Ingresa un valor numérico");
+    }
+    else
+    {
+        /*  Para obtener el valor de un formulario llamo al ATRIBUTO del OBJETO texto
+            y conviento el texto en entero con la función parseInt */
+        var l = 0;
+        var  xi, yi, xf, yf ;
+        var espacio = ancho / lineas; 
+        lienzo.clearRect(0, 0, d.width, d.height);
+        while(l < lineas)
+        {
+            xi = espacio * l;
+            yi = espacio * l;
+            xf = espacio * (l + 1);
+            yf = espacio * (l + 1);
 
-        /*Mando a llamar la función con los parámetros */
-        dibujarLinea("blue", 0, yi, xf, 300);
-        dibujarLinea("blue", xi, 0, 300, yf);
-        l = l + 1;
+            /*Mando a llamar la función con los parámetros */
+            dibujarLinea("blue", 0, yi, xf, 300);
+            dibujarLinea("blue", xi, 0, 300, yf);
+            l = l + 1;
+        }
     }
 }
